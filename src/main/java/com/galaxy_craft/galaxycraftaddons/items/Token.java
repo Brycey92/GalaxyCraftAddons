fb70d534b307f71adebc3173ff0eb13b1637da09
Su5eD <adamko.zachar@gmail.com>
2020-04-17 22:31:32 +0200

    Fix the mod

--- src/main/java/com/galaxy_craft/galaxycraftaddons/items/Token.java
+++ src/main/java/com/galaxy_craft/galaxycraftaddons/items/Token.java
@@ -11,15 +11,18 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class Token extends GCAItem {
 	public static final String PREFIX = "token_tier_";
 	private int tier;
+	private String registryName;
 
-	public Token() {
-		this(0);
-	}
-	
-	public Token(int tokenTier) {
-		super();
+	//a special constructor for the icon
+	public Token(int tokenTier, String registryName) {
 		tier = tokenTier;
-		
+		this.registryName = registryName;
+	}
+
+	public Token(int tokenTier) {
+		//super has no constructor, therefore therefore there is no need to call it
+		tier = tokenTier;
+		registryName = PREFIX+tier;
 	}
 	
 	@Override
@@ -33,8 +36,8 @@ public class Token extends GCAItem {
 	
 	public Item setItemProperties() {
 		setCreativeTab(GalaxyCraftAddons.galaxyCraftAddonsTab);
-		setRegistryName(GalaxyCraftAddons.MODID, PREFIX + tier);
-		setUnlocalizedName(GalaxyCraftAddons.MODID + PREFIX + tier);
+		setRegistryName(GalaxyCraftAddons.MODID, registryName);
+		setUnlocalizedName(GalaxyCraftAddons.MODID + registryName);
 		return this;
 	}
 }
